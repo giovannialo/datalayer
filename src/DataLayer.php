e7163517ba0806e7f5abd62f26eb482c2363dee9
Giovanni A. L. Oliveira <giovanni.al.oliveira@gmail.com>
2021-01-17 11:57:08 -0300

    Fix: o método required() agora permite passar os valores em 0 como int, float, double e string. Não permite permite string vazia ou null.

--- src/DataLayer.php
+++ src/DataLayer.php
@@ -15,386 +15,395 @@ use stdClass;
 class DataLayer
 {
 
-	use CrudTrait;
+    use CrudTrait;
 
-	/**
-	 * @var string
-	 */
-	private $entity;
+    /**
+     * @var string
+     */
+    private $entity;
 
-	/**
-	 * @var array|null
-	 */
-	private $required;
+    /**
+     * @var array|null
+     */
+    private $required;
 
-	/**
-	 * @var string
-	 */
-	private $primary;
+    /**
+     * @var string
+     */
+    private $primary;
 
-	/**
-	 * @var bool|string
-	 */
-	private $timestamps;
+    /**
+     * @var bool|string
+     */
+    private $timestamps;
 
-	/**
-	 * @var string|null
-	 */
-	private $database;
+    /**
+     * @var string|null
+     */
+    private $database;
 
-	/**
-	 * @var string|null
-	 */
-	private $statement;
+    /**
+     * @var string|null
+     */
+    private $statement;
 
-	/**
-	 * @var array|null
-	 */
-	private $params;
+    /**
+     * @var array|null
+     */
+    private $params;
 
-	/**
-	 * @var string|null
-	 */
-	private $group;
+    /**
+     * @var string|null
+     */
+    private $group;
 
-	/**
-	 * @var string|null
-	 */
-	private $order;
+    /**
+     * @var string|null
+     */
+    private $order;
 
-	/**
-	 * @var string|null
-	 */
-	private $limit;
+    /**
+     * @var string|null
+     */
+    private $limit;
 
-	/**
-	 * @var string|null
-	 */
-	private $offset;
+    /**
+     * @var string|null
+     */
+    private $offset;
 
-	/**
-	 * @var string|null
-	 */
-	private $saveMethod;
+    /**
+     * @var string|null
+     */
+    private $saveMethod;
 
-	/**
-	 * @var array|null
-	 */
-	private $functionSql;
+    /**
+     * @var array|null
+     */
+    private $functionSql;
 
-	/**
-	 * @var Exception|PDOException|null
-	 */
-	private $fail;
+    /**
+     * @var Exception|PDOException|null
+     */
+    private $fail;
 
-	/**
-	 * @var object|null
-	 */
-	private $data;
+    /**
+     * @var object|null
+     */
+    private $data;
 
-	/**
-	 * DataLayer constructor.
-	 *
-	 * @param string $entity
-	 * @param array|null $required
-	 * @param string $primary
-	 * @param bool|string $timestamps
-	 * @param string|null $database
-	 */
-	public function __construct(string $entity, ?array $required = [], string $primary = 'id', $timestamps = false, ?string $database = null)
-	{
-		$this->entity = $entity;
-		$this->required = $required;
-		$this->primary = $primary;
-		$this->timestamps = $timestamps;
-		$this->database = $database;
-		$this->functionSql = [];
-	}
+    /**
+     * DataLayer constructor.
+     *
+     * @param string $entity
+     * @param array|null $required
+     * @param string $primary
+     * @param bool|string $timestamps
+     * @param string|null $database
+     */
+    public function __construct(
+        string $entity,
+        ?array $required = [],
+        string $primary = 'id',
+        $timestamps = false,
+        ?string $database = null
+    ) {
+        $this->entity = $entity;
+        $this->required = $required;
+        $this->primary = $primary;
+        $this->timestamps = $timestamps;
+        $this->database = $database;
+        $this->functionSql = [];
+    }
 
-	/**
-	 * @param $name
-	 * @param $value
-	 * @return void
-	 */
-	public function __set($name, $value): void
-	{
-		if (empty($this->data)) {
-			$this->data = new stdClass();
-		}
+    /**
+     * @param $name
+     * @param $value
+     * @return void
+     */
+    public function __set($name, $value): void
+    {
+        if (empty($this->data)) {
+            $this->data = new stdClass();
+        }
 
-		$this->data->$name = $value;
-	}
+        $this->data->$name = $value;
+    }
 
-	/**
-	 * @param $name
-	 * @return bool
-	 */
-	public function __isset($name): bool
-	{
-		return isset($this->data->$name);
-	}
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function __isset($name): bool
+    {
+        return isset($this->data->$name);
+    }
 
-	/**
-	 * @param $name
-	 * @return string|null
-	 */
-	public function __get($name): ?string
-	{
-		$method = $this->toCamelCase($name);
+    /**
+     * @param $name
+     * @return string|null
+     */
+    public function __get($name): ?string
+    {
+        $method = $this->toCamelCase($name);
 
-		if (method_exists($this, $method)) {
-			return $this->$method();
-		}
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
 
-		if (method_exists($this, $name)) {
-			return $this->$name();
-		}
+        if (method_exists($this, $name)) {
+            return $this->$name();
+        }
 
-		return ($this->data->$name ?? null);
-	}
+        return ($this->data->$name ?? null);
+    }
 
-	/**
-	 * @return Exception|PDOException|null
-	 */
-	public function fail()
-	{
-		return $this->fail;
-	}
+    /**
+     * @return Exception|PDOException|null
+     */
+    public function fail()
+    {
+        return $this->fail;
+    }
 
-	/**
-	 * @return object|null
-	 */
-	public function data(): ?object
-	{
-		return $this->data;
-	}
+    /**
+     * @return object|null
+     */
+    public function data(): ?object
+    {
+        return $this->data;
+    }
 
-	/**
-	 * @param string|null $terms
-	 * @param string|null $params
-	 * @param string $columns
-	 * @return DataLayer
-	 */
-	public function find(?string $terms = '', ?string $params = '', string $columns = '*'): DataLayer
-	{
-		if ($terms) {
-			$this->statement = "SELECT {$columns} FROM {$this->entity} WHERE {$terms}";
-			parse_str($params, $this->params);
-			return $this;
-		}
+    /**
+     * @param string|null $terms
+     * @param string|null $params
+     * @param string $columns
+     * @return DataLayer
+     */
+    public function find(?string $terms = '', ?string $params = '', string $columns = '*'): DataLayer
+    {
+        if ($terms) {
+            $this->statement = "SELECT {$columns} FROM {$this->entity} WHERE {$terms}";
+            parse_str($params, $this->params);
+            return $this;
+        }
 
-		$this->statement = "SELECT {$columns} FROM {$this->entity}";
-		return $this;
-	}
+        $this->statement = "SELECT {$columns} FROM {$this->entity}";
+        return $this;
+    }
 
-	/**
-	 * @param string $value
-	 * @param string $columns
-	 * @return DataLayer|null
-	 */
-	public function findByPrimaryKey(string $value, string $columns = '*'): ?DataLayer
-	{
-		return $this->find("{$this->primary} = :{$this->primary}", "{$this->primary}={$value}", $columns)->fetch();
-	}
+    /**
+     * @param string $value
+     * @param string $columns
+     * @return DataLayer|null
+     */
+    public function findByPrimaryKey(string $value, string $columns = '*'): ?DataLayer
+    {
+        return $this->find("{$this->primary} = :{$this->primary}", "{$this->primary}={$value}", $columns)->fetch();
+    }
 
-	/**
-	 * @param string $id
-	 * @param string $columns
-	 * @return DataLayer|null
-	 */
-	public function findById(string $id, string $columns = '*'): ?DataLayer
-	{
-		return $this->find('id = :id', "id={$id}", $columns)->fetch();
-	}
+    /**
+     * @param string $id
+     * @param string $columns
+     * @return DataLayer|null
+     */
+    public function findById(string $id, string $columns = '*'): ?DataLayer
+    {
+        return $this->find('id = :id', "id={$id}", $columns)->fetch();
+    }
 
-	/**
-	 * @param string $group
-	 * @return $this|null
-	 */
-	public function group(string $group): ?DataLayer
-	{
-		$this->group = " GROUP BY {$group}";
-		return $this;
-	}
+    /**
+     * @param string $group
+     * @return $this|null
+     */
+    public function group(string $group): ?DataLayer
+    {
+        $this->group = " GROUP BY {$group}";
+        return $this;
+    }
 
-	/**
-	 * @param string $order
-	 * @return $this|null
-	 */
-	public function order(string $order): ?DataLayer
-	{
-		$this->order = " ORDER BY {$order}";
-		return $this;
-	}
+    /**
+     * @param string $order
+     * @return $this|null
+     */
+    public function order(string $order): ?DataLayer
+    {
+        $this->order = " ORDER BY {$order}";
+        return $this;
+    }
 
-	/**
-	 * @param int $limit
-	 * @return $this|null
-	 */
-	public function limit(int $limit): ?DataLayer
-	{
-		$this->limit = " LIMIT {$limit}";
-		return $this;
-	}
+    /**
+     * @param int $limit
+     * @return $this|null
+     */
+    public function limit(int $limit): ?DataLayer
+    {
+        $this->limit = " LIMIT {$limit}";
+        return $this;
+    }
 
-	/**
-	 * @param int $offset
-	 * @return $this|null
-	 */
-	public function offset(int $offset): ?DataLayer
-	{
-		$this->offset = " OFFSET {$offset}";
-		return $this;
-	}
+    /**
+     * @param int $offset
+     * @return $this|null
+     */
+    public function offset(int $offset): ?DataLayer
+    {
+        $this->offset = " OFFSET {$offset}";
+        return $this;
+    }
 
-	/**
-	 * @param bool $all
-	 * @return DataLayer|mixed|null
-	 */
-	public function fetch(bool $all = false)
-	{
-		try {
-			$connect = Connect::testConnection($this->database);
-			$statement = $connect->prepare($this->statement . $this->group . $this->order . $this->limit . $this->offset);
-			$statement->execute($this->params);
+    /**
+     * @param bool $all
+     * @return DataLayer|mixed|null
+     */
+    public function fetch(bool $all = false)
+    {
+        try {
+            $connect = Connect::testConnection($this->database);
+            $statement = $connect->prepare($this->statement . $this->group . $this->order . $this->limit . $this->offset);
+            $statement->execute($this->params);
 
-			if (!$statement->rowCount()) {
-				return null;
-			}
+            if (!$statement->rowCount()) {
+                return null;
+            }
 
-			if ($all) {
-				return $statement->fetchAll(PDO::FETCH_CLASS, static::class);
-			}
+            if ($all) {
+                return $statement->fetchAll(PDO::FETCH_CLASS, static::class);
+            }
 
-			return $statement->fetchObject(static::class);
-		} catch (PDOException $exception) {
-			$this->fail = $exception;
-			return null;
-		}
-	}
+            return $statement->fetchObject(static::class);
+        } catch (PDOException $exception) {
+            $this->fail = $exception;
+            return null;
+        }
+    }
 
-	/**
-	 * @return int|null
-	 */
-	public function count(): ?int
-	{
-		try {
-			$connect = Connect::testConnection($this->database);
-			$statement = $connect->prepare($this->statement);
-			$statement->execute($this->params);
+    /**
+     * @return int|null
+     */
+    public function count(): ?int
+    {
+        try {
+            $connect = Connect::testConnection($this->database);
+            $statement = $connect->prepare($this->statement);
+            $statement->execute($this->params);
 
-			return $statement->rowCount();
-		} catch (PDOException $exception) {
-			$this->fail = $exception;
-			return null;
-		}
-	}
+            return $statement->rowCount();
+        } catch (PDOException $exception) {
+            $this->fail = $exception;
+            return null;
+        }
+    }
 
-	/**
-	 * @param string $column
-	 * @param string $function
-	 * @return void
-	 */
-	public function functionSql(string $column, string $function): void
-	{
-		$this->functionSql[$column] = $function;
-		$this->$column = $function;
-	}
+    /**
+     * @param string $column
+     * @param string $function
+     * @return void
+     */
+    public function functionSql(string $column, string $function): void
+    {
+        $this->functionSql[$column] = $function;
+        $this->$column = $function;
+    }
 
-	/**
-	 * @return $this|null
-	 */
-	public function make(): ?DataLayer
-	{
-		$this->saveMethod = 'create';
-		return $this;
-	}
+    /**
+     * @return $this|null
+     */
+    public function make(): ?DataLayer
+    {
+        $this->saveMethod = 'create';
+        return $this;
+    }
 
-	/**
-	 * @return $this|null
-	 */
-	public function change(): ?DataLayer
-	{
-		$this->saveMethod = 'update';
-		return $this;
-	}
+    /**
+     * @return $this|null
+     */
+    public function change(): ?DataLayer
+    {
+        $this->saveMethod = 'update';
+        return $this;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function save(): bool
-	{
-		$primary = $this->primary;
-		$value = null;
+    /**
+     * @return bool
+     */
+    public function save(): bool
+    {
+        $primary = $this->primary;
+        $value = null;
 
-		try {
-			if (!$this->required()) {
-				throw new Exception('Preencha os campos necessários');
-			}
+        try {
+            if (!$this->required()) {
+                throw new Exception('Preencha os campos necessários');
+            }
 
-			if ($this->saveMethod === 'update') {
-				$value = $this->data->$primary;
-				$data = (array)$this->data;
+            if ($this->saveMethod === 'update') {
+                $value = $this->data->$primary;
+                $data = (array) $this->data;
 
-				unset($data[$primary]);
+                unset($data[$primary]);
 
-				$this->update($data, "{$primary} = :{$primary}", "{$primary}={$value}");
-			} else {
-				$value = $this->create((array)$this->data);
-			}
+                $this->update($data, "{$primary} = :{$primary}", "{$primary}={$value}");
+            } else {
+                $value = $this->create((array) $this->data);
+            }
 
-			if (!$value) {
-				return false;
-			}
+            if (!$value) {
+                return false;
+            }
 
-			$this->data = $this->findByPrimaryKey($value)->data();
+            $this->data = $this->findByPrimaryKey($value)->data();
 
-			return true;
-		} catch (Exception $exception) {
-			$this->fail = $exception;
-			return false;
-		}
-	}
+            return true;
+        } catch (Exception $exception) {
+            $this->fail = $exception;
+            return false;
+        }
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function destroy(): bool
-	{
-		$primary = $this->primary;
-		$value = $this->data->$primary;
+    /**
+     * @return bool
+     */
+    public function destroy(): bool
+    {
+        $primary = $this->primary;
+        $value = $this->data->$primary;
 
-		if (empty($value)) {
-			return false;
-		}
+        if (empty($value)) {
+            return false;
+        }
 
-		return $this->delete("{$primary} = :{$primary}", "{$primary}={$value}");
-	}
+        return $this->delete("{$primary} = :{$primary}", "{$primary}={$value}");
+    }
 
-	/**
-	 * @return bool
-	 */
-	private function required(): bool
-	{
-		$data = (array)$this->data;
+    /**
+     * @return bool
+     */
+    private function required(): bool
+    {
+        $data = (array) $this->data;
 
-		foreach ($this->required as $field) {
-			if (empty($data[$field])) {
-				return false;
-			}
-		}
+        foreach ($this->required as $field) {
+            if (!isset($data[$field])
+                || is_null($data[$field])
+                || is_string($data[$field])
+                && $data[$field] == ''
+            ) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * @param string $string
-	 * @return string
-	 */
-	private function toCamelCase(string $string): string
-	{
-		$camelCase = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
-		$camelCase[0] = strtolower($camelCase[0]);
-		return $camelCase;
-	}
+    /**
+     * @param string $string
+     * @return string
+     */
+    private function toCamelCase(string $string): string
+    {
+        $camelCase = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
+        $camelCase[0] = strtolower($camelCase[0]);
+        return $camelCase;
+    }
 }
